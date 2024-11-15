dd3d04eb7e58ac1e0b71d6f405f1debc5586ee24
Joaquin Saez <ximosa@gmail.com>
2024-11-15 17:12:49 +0100

    Create html.js

---
+++ html.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Webgae</title>
+    <meta name="description" content="Creamos sitios web bonitos y modernos. Con las mejores herramientas y componentes Rápidos, seguros y optimizados para el SEO.">
+<meta name="keywords" content="experto wordpress, trabajos wordpres, Web Full-Stack">
+  <link href="https://cdn.jsdelivr.net/npm/beercss@3.7.12/dist/cdn/beer.min.css" rel="stylesheet">
+<script type="module" src="https://cdn.jsdelivr.net/npm/beercss@3.7.12/dist/cdn/beer.min.js"></script>
+<script src="script.js"></script>
+</head>
+<body>
+
+  <main class="responsive">
+    holaaaa
+  </main>
+
+  
+ </body>
+ <script type="module" src="main.js"></script>
+</html>
